13c8df6dfa82235e307ea98f0a02e1876e68ce61
Sean Patterson <sean@bosson.design>
2020-05-18 13:15:39 -0400

    initial commit

---
+++ Bulk Density Guide/FavoritesViewController.swift
@@ -0,0 +1,64 @@
+//
+//  FavoritesViewController.swift
+//  Bulk Density Guide
+//
+//  Created by Sean Patterson on 5/8/20.
+//  Copyright © 2020 Bosson Design. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        retrieveArray()
+        convertArray()
+        
+    }
+}
+
+extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return sharedData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = sharedData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath)
+        -> UISwipeActionsConfiguration? {
+            let deleteAction = UIContextualAction(style: .destructive, title: "Remove") { (_, _, completionHandler) in
+                sharedData.remove(at: indexPath.row)
+                saveArray()
+                tableView.beginUpdates()
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+                tableView.endUpdates()
+                completionHandler(true)
+            }
+            if #available(iOS 13.0, *) {
+                deleteAction.image = UIImage(systemName: "trash")
+            } else {
+                // Fallback to default action
+            }
+            deleteAction.backgroundColor = .systemRed
+            let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+            return configuration
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-Bulk Density GuideTests/Pods-Bulk Density GuideTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bulk_Density_GuideTests : NSObject
+@end
+@implementation PodsDummy_Pods_Bulk_Density_GuideTests
+@end
